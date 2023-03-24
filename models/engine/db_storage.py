e9f8ce9d13458e158afe5a57eb0ab7840e772723
Farad078 <faradokorie@gmail.com>
2023-03-24 06:40:30 +0100

    db_storage updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
-from ..models.base_model import BaseModel, Base
+from ..base_model import Base
+from sqlalchemy.orm import sessionmaker, scoped_session
 from os import environ
+from ..city import City
+from ..place import Place
+from ..user import User
+from ..review import Review
+from ..state import State
+
+classes = {'User': "User",
+           'State': "State",
+           'Place': "Place",
+           'Review': "Review",
+           'Amenity': "Amenity"
+           }
 
 
 class DBStorage:
@@ -25,4 +38,36 @@ class DBStorage:
         if environ.get("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
-        def all(self, cls=None):
+    def all(self, cls=None):
+        obj_dict = {}
+        if cls:
+            objs = self.__session.query(classes[cls]).all()
+            for obj in objs:
+                key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                obj_dict[key] = obj
+        else:
+            for i in classes.values():
+                obj_query = self.__session.query(i).all()
+                for obj in obj_query:
+                    key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                    obj_dict[key] = obj
+        return obj_dict
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session)
+        self.__session = Session()
+
+
+
