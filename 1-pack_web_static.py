c4017ca32d011dc42f30e1c8c2d86febd203f24a
SkywalkerDarth <danteuche@gmail.com>
2023-04-06 13:50:28 +0000

    more fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 from fabric.api import local
 from datetime import datetime
 from time import strftime
 
 
 def do_pack():
-    """ A fabric script that generates archive a .tgz archive from the contents of the web_static """
+    """ generates archive a .tgz archie """
 
     local("mkdir -p versions")
     path = ("versions/web_static_{}.tgz".format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
