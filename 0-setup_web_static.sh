289cc772344af8e147a071337d560d535b6ca967
SkywalkerDarth <danteuche@gmail.com>
2023-04-06 13:47:50 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
 # Bash script that sets up a web server
 
-sudo apt-get update
+# install nginx
+sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/releases/test/
+# make directories recursively
 sudo mkdir -p /data/web_static/releases/test/index.html
 sudo mkdir -p /data/web_static/shared/
 
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+# echo message stored in index.html
+echo "<h1>welcome to www.skydar.tech</h1>" > /data/web_static/releases/test/index.html
 
+# prevent overwrite
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+
+# create symbolic link
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R $ubuntu:$ubuntu /data/
+sudo chown -hR $ubuntu:$ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 from fabric.api import local
-from datetime import date
+from datetime import datetime
 from time import strftime
 
 
 def do_pack():
     """ A fabric script that generates archive a .tgz archive from the contents of the web_static """
 
-	filename = strftime("%Y%m%d%H%M%S")
-	try:
-	local("mkdir -p versions")
-	local("tar -czvf versions/web_static_{}.tgz web_static/".format(filename))
-		return "versions/web_static_{}.tgz".format(filename)
-
-	except Exception as a:
-		return None
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz".format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    output = local("tar -cvzf {} web_static".format(path))
+    if output.failed:
+        return None
+        return path
