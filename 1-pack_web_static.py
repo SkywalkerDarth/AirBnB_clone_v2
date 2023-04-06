3c0fcf2675a57ae8bb79ab894ff4753eae207a22
SkywalkerDarth <danteuche@gmail.com>
2023-04-06 14:45:42 +0000

    2-do

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' A Fabric script to generate .tgz archive'''
+'''A Fabric script to generate .tgz archive'''
 
 from fabric.api import local
 from datetime import datetime
@@ -7,7 +7,7 @@ from time import strftime
 
 
 def do_pack():
-    """ generates archive a .tgz archie """
+    '''generates archive a .tgz archie'''
 
     local("mkdir -p versions")
     path = ("versions/web_static_{}.tgz".format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
